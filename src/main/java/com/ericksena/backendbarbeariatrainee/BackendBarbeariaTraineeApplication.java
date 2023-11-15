a8c6c43f31804f16c3830d6fae3412decc3be935
Erick Sena <ericksena621@gmail.com>
2023-11-15 16:21:51 -0300

    feat: setup spring project

---
+++ src/main/java/com/ericksena/backendbarbeariatrainee/BackendBarbeariaTraineeApplication.java
@@ -0,0 +1,13 @@
+package com.ericksena.backendbarbeariatrainee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendBarbeariaTraineeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendBarbeariaTraineeApplication.class, args);
+	}
+
+}
